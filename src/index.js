2b058321bddfd7348b8232f10b7eeab11b18ff0a
valentin-ruggieri <valeru.251@gmail.com>
2022-07-20 15:50:45 -0300

    comando para agregar puerto mediante yargs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+const yargs = require("yargs");
+yargs.command({
+  command: "add port",
+  describe: "add number of port",
+  builder: {
+    PORT: {
+      describe: "number of port",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler(argv) {
+    console.log(`PORT is: ${argv.PORT}`);
+  },
+});
+yargs.parse();
 const app = require("./app");
-const http = require('http')
-const PORT = 8080;
+const http = require("http");
+const PORT = yargs.argv.PORT || 8000;
 const server = http.createServer(app);
-require('./config/configMongoDB')
-require('./utils/passport/passport')
+require("./config/configMongoDB");
+require("./utils/passport/passport");
 server.listen(PORT, () => {
   console.log(`Servidor listo en el puerto ${PORT} ✅`);
 });
