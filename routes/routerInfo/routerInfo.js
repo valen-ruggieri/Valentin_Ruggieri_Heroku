40e9d4f9ce070cc842fd096cbb021dd137005533
valentin-ruggieri <valeru.251@gmail.com>
2022-08-24 00:31:12 -0300

    deploy in heroku

--- routes/routerInfo/routerInfo.js
+++ routes/routerInfo/routerInfo.js
@@ -5,11 +5,13 @@ routerInfo.get("/infobloq", (req, res) => {
   console.log(`GET - ${req.path}`);
   res.send(`GET - ${req.path}`);
 });
+
 routerInfo.get("/infonobloq", (req, res) => {
   res.send(`GET - ${req.path}`);
 });
+
 routerInfo.get("/mensaje", (req, res) => {
-  res.send(`Hola Node.js desde heroku`);
+  res.send(`Hola Node.js desde heroku ✅✅✅`);
 });
 
 
